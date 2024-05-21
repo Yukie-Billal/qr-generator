dca38be185008a9fb0dc9404966dd2cdc369dab3
Yukie M Billal <yukiembillal01@gmail.com>
2024-05-21 08:45:19 +0700

    fix: filename ext

--- generate_qr.py
+++ generate_qr.py
@@ -3,8 +3,8 @@ import os
 from datetime import datetime
 
 def generate_qr_code(text, filename):
-    if len(str(filename).split('')) <= 1:  filename = f"{datetime.now().strftime('%d%m%Y')}-{filename}.png"
-    else: filename = f"{datetime.now().strftime('$d$m$Y')}-{filename}"
+    if len(str(filename).split('.')) <= 1:  filename = f"{datetime.now().strftime('%d%m%Y')}-{filename}.png"
+    else: filename = f"{datetime.now().strftime('%d%m%Y')}-{filename}"
 
     qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=12, border=4)
     qr.add_data(text)
