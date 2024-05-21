88e79af2a5ca3bc5b80940504df0829472a59032
Yukie M Billal <yukiembillal01@gmail.com>
2024-05-21 08:45:53 +0700

    fix: qrcode save dir

--- generate_qr.py
+++ generate_qr.py
@@ -8,11 +8,11 @@ def generate_qr_code(text, filename):
 
     qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=12, border=4)
     qr.add_data(text)
-    if not os.path.exists("public"):
-        os.makedirs("public")
+    if not os.path.exists("static"):
+        os.makedirs("static")
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
 
-    save_path = f"public/{filename}"
+    save_path = f"static/{filename}"
     img.save(save_path)
     return save_path, img
